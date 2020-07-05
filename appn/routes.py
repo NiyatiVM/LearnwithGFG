4d442cc5aaa265467b3fea3666211cb68d0ea541
NiyatiVM <n.mervana@somaiya.edu>
2020-07-05 15:58:06 +0530

    trying changess

--- appn/routes.py
+++ appn/routes.py
@@ -173,18 +173,23 @@ def toLoginp():
     if form.validate_on_submit() and request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        flash(username)
+        flash(password)
+        flash(config('ADMIN_USER'))
+        flash(config('ADMIN_PASSKEY'))
         completion = validate(username, password)
+        flash(completion)
         if completion ==False:
             error = 'Invalid Credentials. Please try again.'
             flash('Invalid Credentials. Please try again.')
-            return render_template('index.html')
+            return redirect(url_for('login.loginp'),form=form)
         else:
         	session['logged_in'] =True
         	session['username'] = username
         	return redirect(url_for('secret.secretp'))
     else:
         flash("Errrrror")
-        return render_template('index.html')
+        return redirect(url_for('login.loginp'),form=form)
     #except:
         #flash("Invalid credentials")
         #return render_template('index.html')
