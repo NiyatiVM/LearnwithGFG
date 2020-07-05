bffee08f532b9093b261b3afd0b2ebf5b411c870
NiyatiVM <n.mervana@somaiya.edu>
2020-07-05 20:45:56 +0530

    changes

--- appn/automate.py
+++ appn/automate.py
@@ -11,19 +11,43 @@ def ps(urlink):
     soupone=BeautifulSoup(response.content,'lxml')
     #results = soupone.find(id=postid)
     results = soupone.find_all('article')
+    resultsforsome=soupone.find_all('div',{'class':'popularArticle'})
     ols=[]
     spans=[]
+    uls=[]
     if len(results)>1:
+      print("Found spans")
       for result in results:
         spans.append(result.find_all("span",{'class':'read-more'}))
+    elif len(resultsforsome)>1:
+      print("Found uls")
+      for result in resultsforsome:
+        exist=result.find("ul")
+        if exist:
+          uls.append(exist)
     else:
+      print("Searching ols")
       ols=results[0].find_all("ol")
-    #print(ols)
+    print(*uls,sep="\n")
     lis=[]
+    liss=[]
     for ol in ols:
       lis.append(ol.findAll('li'))
+    for ul in uls:
+      print("\nfind_all")
+      new=ul.find_all('li')
+      if new:
+        for n in new:
+          liss.append(n)
+      else:
+        pass
+    #print(liss)
     links=[]
+    for i in liss:
+      if i.find('a'):
+        links.append(i.find('a')['href'])
     for l in lis:
+      print(l)
       for i in l:
         if i.find('a'):
           links.append(i.find('a')['href'])
